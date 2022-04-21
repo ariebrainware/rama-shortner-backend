d02af33cb983790bb0c1ee49ea8ce1130827921e
Arie Brainware <arie@rushowl.sg>
2022-04-21 19:47:41 +0700

    Handle cors

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/ariebrainware/rama-shortner-backend
 
-// +heroku goVersion go1.18
 go 1.18
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/gofrs/uuid v4.2.0+incompatible
 	github.com/itchyny/base58-go v0.2.0
@@ -33,7 +33,7 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
 	golang.org/x/text v0.3.5 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -5,15 +5,27 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/ariebrainware/rama-shortner-backend/endpoint"
 	"github.com/ariebrainware/rama-shortner-backend/model"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	r := gin.Default()
+
+	allowedOrigins := os.Getenv("ALLOWED_ORIGIN")
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowWildcard = true
+	corsConfig.AllowOrigins = strings.Split(allowedOrigins, ",") // contain whitelist domain
+	corsConfig.AllowHeaders = []string{"*", "user", "domain", "verb", "object", "Content-Type", "Accept"}
+	corsConfig.AllowCredentials = true
+	corsConfig.AddAllowMethods("OPTIONS")
+	r.Use(cors.New(corsConfig))
+
 	r.GET("/", func(c *gin.Context) {
 		var success string = fmt.Sprintf("Server listening with version %s", os.Getenv("VERSION"))
 		c.JSON(http.StatusOK, &model.Response{
